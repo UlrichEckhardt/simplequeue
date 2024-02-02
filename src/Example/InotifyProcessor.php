213ead94bdb3a65a87dba7e5115b11dd86fb44a2
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-02-02 11:10:18 +0100

    InotifyProcessor: Refactor using the InotifyWatch class

--- src/Example/InotifyProcessor.php
+++ src/Example/InotifyProcessor.php
@@ -25,10 +25,6 @@ use simpleQueue\Job\JobType;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
-if (! extension_loaded('inotify')) {
-    throw new Exception('inotify extension is not loaded');
-}
-
 $configuration = new Configuration();
 
 $factory = new Factory($configuration);
@@ -62,19 +58,11 @@ class Emitter implements IteratorAggregate
     public function getIterator(): Generator
     {
         // configure a watch on the inbox directory
-        $inotify = inotify_init();
-        if ($inotify === false) {
-            throw new Exception('inotify_init() failed');
-        }
-        $watch = inotify_add_watch(
-            $inotify,
+        $inotify = new InotifyWatch();
+        $inotify->watch(
             $this->configuration->getInboxDirectory()->toString(),
             IN_CLOSE_WRITE | IN_MOVED_TO | IN_ATTRIB
         );
-        if ($watch === false) {
-            fclose($inotify);
-            throw new Exception('inotify_add_watch() failed');
-        }
 
         // trigger processing existing jobs
         // Just `touch` all job files once, so that inotify generates an
@@ -89,41 +77,29 @@ class Emitter implements IteratorAggregate
             }
         }
 
-        try {
-            echo posix_getpid().': processing inotify events'.PHP_EOL;
-            while (true) {
-                // wait for an inotify event
-                $events = inotify_read($inotify);
-                if ($events === false) {
-                    throw new Exception('inotify_read() failed');
-                }
-                foreach ($events as $event) {
-                    echo posix_getpid().': inotify event: '.json_encode($event, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR).PHP_EOL;
-                    switch ($event['mask']) {
-                        case IN_ATTRIB:
-                        case IN_CLOSE_WRITE:
-                        case IN_MOVED_TO:
-                            try {
-                                yield $this->read($event['name']);
-                            } catch (Exception $e) {
-                                // TODO: move file
-                                echo posix_getpid().': failed to deserialize event: '.$e->getMessage().PHP_EOL;
-                            }
-                            break;
-                        case IN_IGNORED:
-                            // this seems to occur after processing a job
-                            echo posix_getpid().': ignoring event'.PHP_EOL;
-                            break;
-                        default:
-                            echo posix_getpid().': unexpected event'.PHP_EOL;
-
-                            return;
+        echo posix_getpid().': processing inotify events'.PHP_EOL;
+        foreach ($inotify as $event) {
+            echo posix_getpid().': inotify event: '.json_encode($event, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR).PHP_EOL;
+            switch ($event['mask']) {
+                case IN_ATTRIB:
+                case IN_CLOSE_WRITE:
+                case IN_MOVED_TO:
+                    try {
+                        yield $this->read($event['name']);
+                    } catch (Exception $e) {
+                        // TODO: move file
+                        echo posix_getpid().': failed to deserialize event: '.$e->getMessage().PHP_EOL;
                     }
-                }
+                    break;
+                case IN_IGNORED:
+                    // this seems to occur after processing a job
+                    echo posix_getpid().': ignoring event'.PHP_EOL;
+                    break;
+                default:
+                    echo posix_getpid().': unexpected event'.PHP_EOL;
+
+                    return;
             }
-        } finally {
-            echo posix_getpid().': closing inotify stream'.PHP_EOL;
-            fclose($inotify);
         }
     }
 
